03b20bcb2cfa745ae8b6832996d0825dd16ee9d0
Seung-Gu Lee <awesomeguy4k@gmail.com>
2022-10-18 19:07:45 -0400

    Added missing files

---
+++ studylog/logger-v102.js
@@ -0,0 +1,267 @@
+/* 
+ * JavaScript file for the Study Logger application
+ * 
+ * https://seunggulee.dev/studylog
+ * (Previously: projects.seunggulee.com/studylog)
+ */
+
+
+
+// Declaration of DOM elements
+const recordForm = document.getElementById('main-form');
+const locationInput = document.getElementById("field-location");
+const subjectInput = document.getElementById("field-subject");
+const recordButton = document.getElementById("record-button");
+const cancelButton = document.getElementById("cancel-button");
+
+const timerText = document.getElementById("timer-text");
+const logText = document.getElementById("text-log");
+const timeText = document.getElementById("text-time");
+
+
+// Converts day (0-6, int) to string
+function getDateText(dayNumber)
+{
+    switch(dayNumber) {
+        case 0: return "Sun";
+        case 1: return "Mon";
+        case 2: return "Tues";
+        case 3: return "Wed";
+        case 4: return "Thurs";
+        case 5: return "Fri";
+        case 6: return "Sat";
+    }
+}
+
+// Open and close log popup
+function hideLog()
+{
+    //document.getElementById("log-div").style.display = "none";
+    $("#log-div").fadeOut(200);
+}
+function showLog()
+{
+    //document.getElementById("log-div").style.display = "block";
+    $("#log-div").fadeIn(200);
+}
+
+
+// Start Record
+function recordStart()
+{
+    // Initialize "elapsedTime"
+    if(localStorage.elapsedTime === undefined)
+        localStorage.elapsedTime = 0;
+    
+    // Set Recording to true
+    localStorage.recording = true;
+    
+    // Receive date
+    var date = new Date();
+    localStorage.startDate = date.getTime();
+    
+    // Add date to temp log
+    localStorage.tempLog = "[Start] " +  (date.getMonth() + 1) + "/" 
+            + date.getDate() + " (" + getDateText(date.getDay()) + ") " + date.getHours() + ":" + addLeadingZero(date.getMinutes()) + "<br/>";
+    
+    // Initialize log
+    if(localStorage.log === undefined)
+        localStorage.log = "";
+    
+    
+    
+    
+    
+    // Alert and reload
+    alert("The Start Time was successfully recorded.\n\nFeel free to close the browser or open another tab. Data will be kept. Just don't forget to come back and stop the timer!");
+    
+    location.reload();
+}
+
+function recordEnd()
+{
+    // Reset recording
+    localStorage.removeItem('recording');
+    
+    // Receive date
+    var date = new Date();
+    
+    // Calculate elapsed time
+    var deltaMinutes = Math.ceil((date.getTime() - localStorage.startDate) / (1000 * 60));
+    var currentMinutesTotal = localStorage.elapsedTime;
+    if(localStorage.elapsedTime === undefined)
+        currentMinutesTotal = 0;
+    
+    // Update elapsed log
+    localStorage.elapsedTime = parseInt(currentMinutesTotal) + parseInt(deltaMinutes);
+    
+    // Initialize log
+    var currentLog = localStorage.log;
+    if(currentLog === undefined || currentLog === null) {
+        currentLog = "";
+    }
+    
+    // Get location and subject
+    var loc = locationInput.value;
+    var subject = subjectInput.value;
+    
+    // Update log
+    if(loc !== "" || subject !== "") // has info to add
+    {
+        localStorage.log = currentLog + localStorage.tempLog + "[End] " +  (date.getMonth() + 1) + "/" 
+                + date.getDate() + " (" + getDateText(date.getDay()) + ") " + date.getHours() 
+                + ":" + addLeadingZero(date.getMinutes()) + " (" + convertMinutesToString(deltaMinutes) +")<br/>"
+                + "[Info] Location: " + loc + " / Subject: " + subject + "<br/>";
+    }
+    else {
+        localStorage.log = currentLog + localStorage.tempLog + "[End] " +  (date.getMonth() + 1) + "/" 
+                + date.getDate() + " (" + getDateText(date.getDay()) + ") " + date.getHours() 
+                + ":" + addLeadingZero(date.getMinutes()) + " (" + convertMinutesToString(deltaMinutes) +")<br/>";
+    }
+        
+    
+    localStorage.tempLog = "";
+    
+    
+    
+    
+    
+    // Comment
+    var comment = prompt("The End Time was successfully recorded." +
+            "\nYou studied for " + convertMinutesToString(deltaMinutes) + "!\n\nDo you have any comments to add?", "");
+    
+    currentLog = localStorage.log;
+    if(comment !== "" && comment !== null) {
+        localStorage.log = currentLog + "[Comment] " + comment  + "<br/><br/>";
+        alert("Success!");
+    }
+    else {
+        localStorage.log = currentLog + "<br/>";
+    }
+        
+    
+    location.reload();
+}
+
+function record()
+{
+    if(localStorage.recording !== undefined) {
+        recordEnd();
+    }
+    else {
+        recordStart();
+    }
+}
+
+function convertMinutesToString(minutes)
+{
+    var hours = Math.floor(minutes / 60);
+    var mins = minutes % 60;
+    
+    return "" + hours + " hr " + mins + " min";
+}
+
+function addLeadingZero(minutes)
+{
+    if(minutes < 10)
+        return "0" + minutes;
+    else
+        return minutes;
+}
+
+
+
+
+
+
+function askReset()
+{
+    let result = confirm("Are you sure you want to delete all saved data? This will also cancel the current on-going record. This cannot be undone.");
+    
+    if(result === true) {
+        resetData();
+    }
+}
+
+function resetData()
+{
+    localStorage.clear();
+    alert("The data has been cleared.");
+    location.reload();
+}
+
+function printLog()
+{
+    let log = localStorage.log;
+    
+    var elapsedTime = "<br/>Total Study Time: " + convertMinutesToString(localStorage.elapsedTime);
+    
+    if(log === undefined) {
+        logText.innerHTML = "Your Study Log is empty. Start studying now!";
+    }
+    else {
+        logText.innerHTML = log + localStorage.tempLog + "<br/>= End of Log =";
+        timeText.innerHTML = elapsedTime;
+    }
+    
+    
+}
+
+
+
+function testLocalStorageSave()
+{
+    localStorage.log = "Test Log AAAAhhhhh";
+    alert("Log saved");
+    location.reload();
+}
+
+function init()
+{
+    console.log("localStorage.recording => " + localStorage.recording);
+    
+    
+    if(localStorage.recording !== undefined) // Recording
+    {
+        recordButton.innerHTML = "■";
+        
+        recordButton.style.backgroundColor = "#ff9966";
+        updateTimer();
+        setInterval(updateTimer, 3000);
+    }
+    else // Not recording
+    {
+        recordButton.innerHTML = "▶";
+        recordButton.style.backgroundColor = "#009933";
+        cancelButton.style.display = "none";
+        locationInput.style.display = "none";
+        subjectInput.style.display = "none";
+    }
+    
+    printLog();
+}
+
+function updateTimer()
+{
+    var date = new Date();
+    var dateDifference = (date.getTime() - localStorage.startDate);
+    var deltaMinutes = Math.ceil(dateDifference / (1000 * 60));
+    timerText.innerHTML = "🕒 " + convertMinutesToString(deltaMinutes - 1);
+}
+
+
+function cancelRecord()
+{
+    let result = confirm("Are you sure you want to cancel the current record?");
+    
+    if(result === true) {
+        
+        localStorage.removeItem('recording');
+        localStorage.tempLog = "";
+        
+        alert("The record was successfully cancelled.");
+        location.reload();
+    }
+}
+
+init();
